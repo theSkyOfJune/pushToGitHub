dbdac68600df77b27524a4eccce706b4f6551d9b
theSkyOfJune <1756088050@qq.com>
2016-09-09 16:05:32 +0800

    新建项目
    初始化项目

---
+++ pushToGitHub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pushToGitHub
+//
+//  Created by HHIOS on 16/9/9.
+//  Copyright © 2016年 LiYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ pushToGitHub/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  pushToGitHub
+//
+//  Created by HHIOS on 16/9/9.
+//  Copyright © 2016年 LiYang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
